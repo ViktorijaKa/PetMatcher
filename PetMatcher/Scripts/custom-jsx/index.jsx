7e2ebf97aafd00b609c0d634182035db6a41e2ca
Viktorija Kažeivaitė <viktorija@quatrodev.com>
2017-02-18 00:19:20 +0200

    Add SASS, First Try WIth React.js

---
+++ PetMatcher/Scripts/custom-jsx/index.jsx
@@ -0,0 +1,10 @@
+﻿var HomePage = React.createClass({
+    render: function () {
+        return (
+            <div>Hello {this.props.name}</div>
+            );
+    }
+
+});
+
+React.render( <HomePage name="World" />, document.getElementById('container'));
